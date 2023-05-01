1820f1dfddf0c95709718e6a1058e4541aa35b77
Kovács Bálint Hunor <kovacsbalinthunor53@gmail.com>
2023-05-01 16:23:07 +0300

    Update pdf-merge.py

--- pdf-merge.py
+++ pdf-merge.py
@@ -3,7 +3,7 @@
 from pypdf import PdfMerger 
 import sys 
 
-if len(sys.argv) < 1:
+if len(sys.argv) < 2:
     print("Usage: ",sys.argv[0], "/path/to/filename.pdf path/to/filename2.pdf ...")
     exit(-1)
 
